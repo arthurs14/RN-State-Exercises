d2026042496b893a6b99ae65c3c88e5edb552a62
arthurs14 <arthurcs426@gmail.com>
2020-04-07 16:31:38 -0700

    simple react hook implemented on a counter screen

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, View, Text, Button } from 'react-native';
 
 const CounterScreen = () => {
+  let [counter, setCounter] = useState(0); // set initial state
+
   return (
     <View>
-      <Text>Hello from Counter Screen!</Text>
-
+      <Button
+        title="Increase"
+        onPress={() => {
+          setCounter(counter + 1);
+        }}
+      />
+      <Button
+        title="Decrease"
+        onPress={() => {
+          setCounter(counter - 1);
+        }}
+      />
+      <Text>Current Count: {counter}</Text>
+      <Text>Simple use of state using React Hooks at its basic form!</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({});
 
 export default CounterScreen;
